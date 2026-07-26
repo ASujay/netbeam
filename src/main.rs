53a6d39f9867ab0d6d07297df966a1849ca0b94e
ASujay <aniketsujay110@gmail.com>
2026-07-26 20:03:19 +0530

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,187 @@
-use std::sync::Arc;
-use std::{thread};
-use crate::error::NetbeamResult;
-use crate::packet::{Packet, DiscoveryAckPacket};
-use crate::udp::{UdpNode};
-use crate::utils::{get_hostname, invalid_cmd_err, usage_err};
+use std::{env, io, mem, net::{IpAddr::{self, V4, V6}, SocketAddr, UdpSocket}, process::exit, thread, time::{self, Duration}};
+use network_interface::{NetworkInterface, NetworkInterfaceConfig};
+use rand::{RngExt, rng};
 
-mod udp;
-mod tcp;
-mod utils;
-mod error;
-mod packet;
+const DEFAULT_UDP_PORT: u16 = 11665;
+const DEFAULT_TCP_PORT: u16 = 11666;
+const DEFAULT_UDP_IP: &str = "0.0.0.0";
+const DEFAULT_MAX_RETRIES: u16 = 20;
 
-fn send(file_path: &String) -> NetbeamResult<()> {
-    _ = file_path;
-    let mut listener = UdpNode::new(None)?;
-    listener.set_broadcast()?;
-    println!("Sending discovery packet");
-    // we will send the discovery packet
-    listener.send_packet(Packet::Discovery, &listener.broadcast_addr)?;
+type NBResult<T> = Result<T, NBError>; 
 
-    let udp_handle = thread::spawn(move ||{
+#[derive(Debug)]
+enum NBError {
+    TcpSocketBindFail,
+    UdpSocketBindFail(io::Error),
+    NetworkInterfaceError(network_interface::Error),
+}
+
+impl From<io::Error> for NBError {
+    fn from(value: io::Error) -> Self {
+        NBError::UdpSocketBindFail(value)
+    }
+}
+
+impl From<network_interface::Error> for NBError {
+    fn from(value: network_interface::Error) -> Self {
+        NBError::NetworkInterfaceError(value)
+    }
+}
+
+
+enum UdpProtocolPacket {
+    Conn,
+    Info(u16, u64),
+    Ack,
+}
+
+impl UdpProtocolPacket {
+    fn create_packet(&self) -> String {
+        match self {
+            UdpProtocolPacket::Conn => String::from("CONN"),
+            UdpProtocolPacket::Info(port, request_id) => String::from("INFO"),
+            UdpProtocolPacket::Ack => String::from("ACK"),
+        }
+    }
+}
+
+
+#[derive(PartialEq)]
+enum UdpCommMode {
+    Send,
+    Receive,
+}
+
+struct UdpComm {
+    socket: UdpSocket,
+    mode: UdpCommMode,
+}
+
+impl UdpComm {
+    fn new(mode: UdpCommMode) -> NBResult<UdpComm> {
+        let socket = UdpSocket::bind(&format!("{}:{}", DEFAULT_UDP_IP, DEFAULT_UDP_PORT))?;
+        if mode == UdpCommMode::Send {
+            socket.set_broadcast(true)?;
+        }
+        Ok(UdpComm {socket, mode})
+    }
+
+    fn send(&self) -> NBResult<()>{
+        let interfaces = NetworkInterface::show()?;
+        let mut broadcast_addrs: Vec<IpAddr> = Vec::new();
+        for interface in interfaces {
+            // collect all the address that we can send a broadcast to
+            for addr in interface.addr {
+                if let Some(addr) = addr.broadcast() && !addr.is_loopback(){
+                    broadcast_addrs.push(addr);
+                }
+            }
+        }
+
+        let socket = self.socket.try_clone()?;
+        // we will send broadcase and we will listen for acknowledgement
+        let boardcast_handle = thread::spawn(move || {
+            loop {
+                for ip in &broadcast_addrs {
+                    // send a CONN broadcast to the subnet
+                    // TODO(Aniket): Fix this later, we should check what error we are getting
+                    _ = socket.send_to(UdpProtocolPacket::Conn.create_packet().as_bytes(), SocketAddr::new(*ip, DEFAULT_UDP_PORT));
+                    
+                }
+                // wait for 5 secs to resend
+                thread::sleep(Duration::from_secs(5));
+            }
+        });
+
+        let info_receiver_handle = thread::spawn(|| {
+        });
+
+        _ = boardcast_handle.join();
+        _ = info_receiver_handle.join();
+        Ok(())
+    }
+
+    fn receive(&self) -> NBResult<()>{
+        let socket = self.socket.try_clone()?;
+        let conn_handler_handle = thread::spawn(move || {
+            loop {
+                let mut buf = [0u8; 1024];
+                if let Ok((bytes_read, socket_addr)) = socket.recv_from(&mut buf) {
+                    if &buf[0..bytes_read] == b"CONN" {
+                        println!("CONNECTION REQUEST: {}", socket_addr.ip());
+                    }
+                }
+            }
+        });
+
+        let ack_sender_handle = thread::spawn(|| {
+
+        });
+
+
+        _ = conn_handler_handle.join();
+        _ = ack_sender_handle.join();
+        Ok(())
+    }
+
+    fn run(&self) -> NBResult<()> {
+        match self.mode {
+            UdpCommMode::Send => Ok(self.send()?),
+            UdpCommMode::Receive => Ok(self.receive()?
+        ),
+        }
+    }
+}
+
+struct TCPComm {}
+
+impl TCPComm {
+    fn new() -> NBResult<TCPComm> {
+        Ok(TCPComm {})   
+    }
+
+    fn retry_until_bind(ip: &str, port: u16) -> NBResult<UdpSocket> {
+        let mut port = port;
+        let mut retry_count = 0;
         loop {
-            let mut buf = [0u8; 1024];
-            match listener.socket.recv_from(&mut buf) {
-                Ok((size, _)) => {
-                    // here we deserialize the data
-                    if buf[0] == 0 && let Packet::DiscoveryAck(packet) = Packet::deserialize_discovery_ack_packet(&buf[1..], size){
-                        println!("{:?}", size);
-                        println!("{:?}", packet);
+            match UdpSocket::bind(&format!("{}:{}", ip, port)) {
+                Ok(socket) => return Ok(socket),
+                Err(e) => {
+                    if retry_count < DEFAULT_MAX_RETRIES {
+                        eprintln!("Failed to bind socket: {}. Retrying...", e);
+                        port += rng().random_range(1..10);
+                        retry_count += 1;
+                    } else {
+                        return Err(NBError::TcpSocketBindFail);
                     }
                 },
-                Err(err) => {},
-            }
+            };
         }
-    });
-    _ = udp_handle.join();    
-    Ok(())
-}
-
-fn receive() -> NetbeamResult<()>{
-    let listener = UdpNode::new(None)?; 
-    let mut buf = [0u8;1024];
-    let hostname = Arc::new(get_hostname()?);
-    println!("Listening to connections...");
-    let udp_handle = thread::spawn(move || {
-        let hostname = Arc::clone(&hostname);
-        let (size, src_addr) = listener.socket.recv_from(&mut buf).unwrap();
-        let scan_message = String::from_utf8_lossy(&buf[..size]);
-        println!("{}", scan_message);
-        println!("{}", src_addr);
-        println!("Sending the acknowledgement");
-        let result = listener.send_packet(Packet::DiscoveryAck(DiscoveryAckPacket{
-            hostname: (*hostname).clone(),
-            ip: listener.ip,
-        }), &src_addr.to_string());
-        match result {
-            Ok(()) => {},
-            Err(err) => {
-                println!("{:?}", err);
-            },
-        }
-    });
-
-    let tcp_handle = thread::spawn(move ||{
-        loop {}
-    });
-    _ = udp_handle.join();
-    _ = tcp_handle.join();
-    Ok(())
-}
-
-fn main() -> NetbeamResult<()> {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() == 1 {
-        usage_err();
     }
+}
+
+fn main() -> NBResult<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Please provide operation mode!");
+        eprintln!("Usage netbeam <send|receive>");
+        exit(1);
+    }
+
+    let mut mode: Option<UdpCommMode> = None;
     match args[1].as_str() {
-        "send" => {
-            if args.len() == 3 {
-                send(&args[2])?;
-            } else {
-                usage_err();
-            }
-        },
-        "receive" => receive()?,
-        _ => invalid_cmd_err(&args[1]),
+        "send" => {mode = Some(UdpCommMode::Send);},
+        "receive" => {mode = Some(UdpCommMode::Receive);},
+        _ => {},
     };
+
+    if let Some(mode) = mode {
+        let udp_comm = UdpComm::new(mode)?;
+        udp_comm.run();
+    } else {
+        eprintln!("Invalid mode provided");
+        eprintln!("Usage netbeam <send|receive>");
+        exit(1);
+    }
     Ok(())
 }
