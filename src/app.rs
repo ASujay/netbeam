25a9e4e17a4c4f0b6c4ae97302a4980810dac6de
ASujay <aniketsujay110@gmail.com>
2026-08-04 19:00:37 +0530

    Finally fixed the receiver part

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,15 @@
-use std::sync::{Arc, Mutex};
-use std::time::Duration;
-use std::{env, thread};
-use std::net::{IpAddr, SocketAddr, UdpSocket};
-use std::sync::mpsc::{self};
-use network_interface::{NetworkInterface, NetworkInterfaceConfig};
 use crate::common::{DEFAULT_TCP_PORT, DEFAULT_UDP_PORT, UDP_SOCKET_ADDRESS};
 use crate::device::{Device, DeviceRegistry};
 use crate::errors::{NBError, NBResult};
 use crate::event::{EventManager, Events};
 use crate::packet::DiscoveryPacket;
 use crate::thread::{ShutdownSignal, ThreadContext, ThreadGroup};
+use network_interface::{NetworkInterface, NetworkInterfaceConfig};
+use std::net::{IpAddr, SocketAddr, UdpSocket};
+use std::sync::mpsc::{self};
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use std::{env, thread};
 
 pub struct App {
     sender: FileSender,
@@ -27,8 +27,8 @@ impl App {
         let thread_context = ThreadContext::new(shutdown, event_sender);
         let sender_socket_address = format!("{}:{}", UDP_SOCKET_ADDRESS, DEFAULT_UDP_PORT);
         let socket = UdpSocket::bind(sender_socket_address)?;
-        Ok(App { 
-            sender: FileSender::new(thread_context.clone(), &socket)?, 
+        Ok(App {
+            sender: FileSender::new(thread_context.clone(), &socket)?,
             receiver: FileReceiver::new(thread_context.clone())?,
             event_manager: EventManager::new(event_sreceiver),
             state: AppState::new(),
@@ -41,7 +41,9 @@ impl App {
         // validate the command line argument
         // usage should be netbeam <send|receive>
         let args: Vec<String> = env::args().collect();
-        if args.len() != 2 { return Err(NBError::InvalidCommandLineArgs); }
+        if args.len() != 2 {
+            return Err(NBError::InvalidCommandLineArgs);
+        }
         let app_thread_group = match args[1].as_str() {
             "send" => self.sender.run(&self.socket)?,
             "receive" => self.receiver.run(&self.socket)?,
@@ -57,12 +59,11 @@ impl App {
 
     pub fn process_events(&mut self) {
         self.event_manager.process_events(&mut self.state);
-    }   
+    }
 }
 
 struct FileSender {
     context: ThreadContext,
-
 }
 
 impl FileSender {
@@ -81,7 +82,9 @@ impl FileSender {
             for interface in interfaces {
                 // collect all the address that we can send a broadcast to
                 for addr in interface.addr {
-                    if let Some(addr) = addr.broadcast() && !addr.is_loopback(){
+                    if let Some(addr) = addr.broadcast()
+                        && !addr.is_loopback()
+                    {
                         broadcast_addrs.push(addr);
                     }
                 }
@@ -89,7 +92,10 @@ impl FileSender {
             while !broadcast_thread_context.is_shutdown() {
                 println!("Sending boardcast");
                 for ip in &broadcast_addrs {
-                    socket.send_to(DiscoveryPacket::Conn.encode().as_slice(), SocketAddr::new(*ip, DEFAULT_UDP_PORT))?;
+                    socket.send_to(
+                        DiscoveryPacket::Conn.encode().as_slice(),
+                        SocketAddr::new(*ip, DEFAULT_UDP_PORT),
+                    )?;
                 }
                 thread::sleep(Duration::from_secs(5));
             }
@@ -132,17 +138,23 @@ impl FileReceiver {
                 let (bytes_read, socket_address) = socket.recv_from(&mut buf)?;
                 let packet = DiscoveryPacket::decode(&buf[0..bytes_read]);
                 if let Some(packet) = packet {
-                    println!("{:?}", packet);
                     match packet {
                         DiscoveryPacket::Conn => {
                             // we need to send the reply to the sender
-                            let packet = DiscoveryPacket::Info { port: DEFAULT_TCP_PORT, request_id: 0xFF }.encode();
-                            socket.send_to(packet.as_slice(), socket_address)?;
-                        },
-                        DiscoveryPacket::Ackn{request_id} => {},
-                        _ => {},
+                            let packet = DiscoveryPacket::Info {
+                                port: DEFAULT_TCP_PORT,
+                                request_id: 0xFF,
+                            }
+                            .encode();
+                            println!("{:?}", packet);
+                            if let Err(e) = socket.send_to(packet.as_slice(), socket_address) {
+                                eprint!("Error replying to broadcaster");
+                            }
+                        }
+                        DiscoveryPacket::Ackn { request_id } => {}
+                        _ => {}
                     }
-                } 
+                }
             }
             Ok(())
         });
@@ -157,7 +169,7 @@ pub struct AppState {
 
 impl AppState {
     fn new() -> Self {
-        AppState { 
+        AppState {
             is_running: true,
             registry: Arc::new(Mutex::new(DeviceRegistry::new())),
         }
